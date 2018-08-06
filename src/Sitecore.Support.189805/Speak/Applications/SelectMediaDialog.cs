719e520fc29433b8accd6932960504820fd2152b
DK\IRO <iro@sitecore.net>
2018-08-06 11:31:32 +0300

    Initial to fix in EE

---
+++ src/Sitecore.Support.189805/Speak/Applications/SelectMediaDialog.cs
@@ -0,0 +1,47 @@
+﻿namespace Sitecore.Support.Speak.Applications
+{
+    using Sitecore;
+    using Sitecore.Data.Items;
+    using Sitecore.Web;
+    using System.Web;
+
+    public class SelectMediaDialog : Sitecore.Speak.Applications.SelectMediaDialog
+    {
+        public override void Initialize()
+        {
+            RedirectOnItembucketsDisabled(ClientHost.Items.GetItem("{16227E67-F9CB-4FB7-9928-7FF6A529708E}"));
+            string queryString = WebUtil.GetQueryString("ro");
+            string queryString2 = WebUtil.GetQueryString("fo");
+            bool showFullPath = GetShowFullPath(queryString2);
+            string queryString3 = WebUtil.GetQueryString("hasUploaded");
+            if (!string.IsNullOrEmpty(queryString3) && queryString3 == "1")
+            {
+                DataSource.Parameters["SearchConfigItemId"] = "{1E723604-BFE0-47F6-B7C5-3E2FA6DD70BD}";
+                Menu.Parameters["DefaultSelectedItemId"] = "{BE8CD31C-2A01-4ED6-9C83-E84C2275E429}";
+            }
+            Item mediaItemFromQueryString = GetMediaItemFromQueryString(queryString);
+            mediaItemFromQueryString = GetRootItem(mediaItemFromQueryString);
+            Item mediaItemFromQueryString2 = GetMediaItemFromQueryString(queryString2);
+            if (mediaItemFromQueryString != null)
+            {
+                DataSource.Parameters["RootItemId"] = mediaItemFromQueryString.ID.ToString();
+            }
+            if (mediaItemFromQueryString == null)
+            {
+                mediaItemFromQueryString = ClientHost.Items.GetItem("/sitecore/media library");
+            }
+            MediaResultsListControl.Parameters["ContentLanguage"] = mediaItemFromQueryString.Language.ToString();
+            MediaResultsListControl.Parameters["DefaultSelectedItemId"] = ((mediaItemFromQueryString2 == null) ? mediaItemFromQueryString.ID.ToString() : mediaItemFromQueryString2.ID.ToString());
+            MediaFolderValueText.Parameters["Text"] = GetDisplayPath(mediaItemFromQueryString.Paths.Path, null, showFullPath);
+            TreeViewToggleButton.Parameters["Click"] = string.Format(TreeViewToggleButton.Parameters["Click"], HttpUtility.UrlEncode(queryString), HttpUtility.UrlEncode(queryString2), showFullPath);
+            string url;
+            string before;
+            string after;
+            string separator;
+            FillCommandParts(UploadButton.Parameters["Click"], out url, out before, out after, out separator);
+            string text = SetUrlContentDatabase(url, WebUtil.GetQueryString("sc_content"));
+            string format = before + separator + text + separator + after;
+            UploadButton.Parameters["Click"] = string.Format(format, HttpUtility.UrlEncode(queryString), HttpUtility.UrlEncode(queryString2), showFullPath);
+        }
+    }
+}
---
+++ src/Sitecore.Support.189805/XA/Foundation/Editing/Commands/ChooseImage.cs
@@ -0,0 +1,126 @@
+﻿namespace Sitecore.Support.XA.Foundation.Editing.Commands
+{
+    using Microsoft.Extensions.DependencyInjection;
+    using Sitecore;
+    using Sitecore.Data.Fields;
+    using Sitecore.Data.Items;
+    using Sitecore.DependencyInjection;
+    using Sitecore.Diagnostics;
+    using Sitecore.Exceptions;
+    using Sitecore.Globalization;
+    using Sitecore.Shell.Applications.ContentEditor;
+    using Sitecore.Shell.Applications.Dialogs.MediaBrowser;
+    using Sitecore.Shell.Applications.WebEdit.Commands;
+    using Sitecore.Shell.Framework.Commands;
+    using Sitecore.Web;
+    using Sitecore.Web.UI.Sheer;
+    using Sitecore.XA.Foundation.Abstractions;
+    using System;
+    using System.Linq;
+
+    [Serializable]
+    public class ChooseImage : Sitecore.XA.Foundation.Editing.Commands.ChooseImage
+    {
+        public override void Execute(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            WebEditCommand.ExplodeParameters(context);
+            string formValue = WebUtil.GetFormValue("scPlainValue");
+            context.Parameters.Add("fieldValue", formValue);
+            if (context.Items.Any())
+            {
+                context.Parameters.Add("contextItem", context.Items.First().ID.ToString());
+            }
+            ServiceLocator.ServiceProvider.GetService<IContext>().ClientPage.Start(this, "Run", context.Parameters);
+        }
+
+        protected override void Run(ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull(args, "args");
+            Item itemNotNull = Client.GetItemNotNull(args.Parameters["itemid"], Language.Parse(args.Parameters["language"]));
+            itemNotNull.Fields.ReadAll();
+            Field field = itemNotNull.Fields[args.Parameters["fieldid"]];
+            Assert.IsNotNull(field, "field");
+            ImageField imageField = new ImageField(field, field.Value);
+            string str = args.Parameters["controlid"];
+            if (args.IsPostBack)
+            {
+                if (args.Result != "undefined")
+                {
+                    string value;
+                    if (!string.IsNullOrEmpty(args.Result))
+                    {
+                        MediaItem mediaItem = Client.ContentDatabase.Items[args.Result];
+                        if (mediaItem != null)
+                        {
+                            imageField.SetAttribute("mediaid", mediaItem.ID.ToString());
+                            if (!string.IsNullOrEmpty(args.Parameters["fieldValue"]))
+                            {
+                                XmlValue xmlValue = new XmlValue(args.Parameters["fieldValue"], "image");
+                                string attribute = xmlValue.GetAttribute("height");
+                                if (!string.IsNullOrEmpty(attribute))
+                                {
+                                    imageField.Height = attribute;
+                                }
+                                string attribute2 = xmlValue.GetAttribute("width");
+                                if (!string.IsNullOrEmpty(attribute2))
+                                {
+                                    imageField.Width = attribute2;
+                                }
+                            }
+                        }
+                        else
+                        {
+                            SheerResponse.Alert("Item not found.", Array.Empty<string>());
+                        }
+                        value = imageField.Value;
+                    }
+                    else
+                    {
+                        value = string.Empty;
+                    }
+                    string value2 = WebEditImageCommand.RenderImage(args, value);
+                    SheerResponse.SetAttribute("scHtmlValue", "value", value2);
+                    SheerResponse.SetAttribute("scPlainValue", "value", value);
+                    SheerResponse.Eval("scSetHtmlValue('" + str + "')");
+                }
+            }
+            else
+            {
+                string text = StringUtil.GetString(ResolveSourceQuery(field.Source, itemNotNull), "/sitecore/media library");
+                string text2 = imageField.GetAttribute("mediaid");
+                if (text.StartsWith("~", StringComparison.InvariantCulture))
+                {
+                    if (string.IsNullOrEmpty(text2))
+                    {
+                        text2 = StringUtil.Mid(text, 1);
+                    }
+                    text = "/sitecore/media library";
+                }
+                Language language = itemNotNull.Language;
+                MediaBrowserOptions mediaBrowserOptions = new MediaBrowserOptions();
+                Item item = Client.ContentDatabase.GetItem(text, language);
+                if (item == null)
+                {
+                    throw new ClientAlertException("The source of this Image field points to an item that does not exist.");
+                }
+                mediaBrowserOptions.Root = item;
+                mediaBrowserOptions.AllowEmpty = true;
+                #region Added code
+               // mediaBrowserOptions.SelectedItem = itemNotNull;
+                #endregion
+                SelectSiteMediaRoot(args, mediaBrowserOptions);
+                if (!string.IsNullOrEmpty(text2))
+                {
+                    Item item2 = Client.ContentDatabase.GetItem(text2, language);
+                    if (item2 != null)
+                    {
+                        mediaBrowserOptions.SelectedItem = item2;
+                    }
+                }
+                SheerResponse.ShowModalDialog(mediaBrowserOptions.ToUrlString().ToString(), "1200px", "700px", string.Empty, true);
+                args.WaitForPostBack();
+            }
+        }
+    }
+}
